63579510d54c77a0b21117a63e5cc8931710be7d
Nelson Guerra <nelsonguerra.tech@outlook.com>
2022-07-05 14:21:15 -0400

    Moving into express

--- node-express-course-main/02-express-tutorial/final/03-express-basics.js
+++ node-express-course-main/02-express-tutorial/final/03-express-basics.js
@@ -14,10 +14,11 @@ app.all('*', (req, res) => {
   res.status(404).send('<h1>resource not found</h1>');
 });
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000...');
+app.listen(3000, () => {
+  console.log('server is listening on port 3000...');
 });
 
+// * HTTP Verbs
 // app.get
 // app.post
 // app.put
